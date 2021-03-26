1dadff9e801a54247b5f83c0e92cfd8becc62ec3
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-03-26 23:42:21 +0100

    Unslash Config Security

--- src/main/java/com/spacefinance/spacefinance/security/SecurityConfiguration.java
+++ src/main/java/com/spacefinance/spacefinance/security/SecurityConfiguration.java
@@ -1,29 +1,29 @@
-package com.spacefinance.spacefinance.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-@Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("Kasia").password("{noop}mysza300").roles("USER","ADMIN")
-                .and()
-                .withUser("Kamil").password("{noop}mysza300").roles("USER","ADMIN");
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-//               .antMatchers("/","/showNewNoteForm","/about","/assets/**").permitAll()
-                .antMatchers("/**","/paragon_update").hasRole("ADMIN")
-                .antMatchers("/**","/paragon_update").hasRole("USER")
-                .anyRequest().authenticated()
-                .and().formLogin().defaultSuccessUrl("/")
-                .and().logout().logoutSuccessUrl("/");
-    }
-}
+//package com.spacefinance.spacefinance.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//@Configuration
+//@EnableGlobalMethodSecurity(securedEnabled = true)
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("Kasia").password("{noop}mysza300").roles("USER","ADMIN")
+//                .and()
+//                .withUser("Kamil").password("{noop}mysza300").roles("USER","ADMIN");
+//    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+////               .antMatchers("/","/showNewNoteForm","/about","/assets/**").permitAll()
+//                .antMatchers("/**","/paragon_update").hasRole("ADMIN")
+//                .antMatchers("/**","/paragon_update").hasRole("USER")
+//                .anyRequest().authenticated()
+//                .and().formLogin().defaultSuccessUrl("/")
+//                .and().logout().logoutSuccessUrl("/");
+//    }
+//}
