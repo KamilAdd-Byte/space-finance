d991f743fb7a286f1a951b92d6611f54a8c004a2
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-03-27 01:10:20 +0100

    Security on and add img to index

---
+++ src/main/java/com/spacefinance/spacefinance/controller/TravelController.java
@@ -0,0 +1,10 @@
+//package com.spacefinance.spacefinance.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class TravelController {
+//    @GetMapping("/travel")
+//    public String travel()
+//}
